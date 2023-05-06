a808a7d631743a3e6c06a4e7e746ed99a2b80560
Sumeet S <sumeets@Sumeets-MacBook-Pro.local>
2023-05-06 11:54:45 +0530

    Add tf code to create ec2

--- main.tf
+++ main.tf
@@ -11,3 +11,29 @@ resource "aws_instance" "ec2_instance" {
     instance_type = "${var.instance_type}"
     key_name = "${var.ami_key_pair_name}"
 } 
+
+resource "aws_security_group" "demo-sg" {
+  name = “sec-grp”
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
